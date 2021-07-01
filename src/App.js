cdb34e3f546ba2185a881bc85324fcaf72f44193
Nikish16 <52878544+Nikish16@users.noreply.github.com>
2021-07-01 18:39:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-
-
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {apiResponce: ""};
-  }
-
-  callAPI(){
-    fatch("http://localhost:3000/testAPI")
-    .then(res => res.text())
-    .then(res => this.setState({apiResponce: res}))
-  }
-
-  ComponentWillMount(){
-    this.callAPI();
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.starte.apiResponce} </p>
-      </header>
+function App(){
+  return(   
+    <div>
+      <h1>
+        Brightroots Project
+        <br>
+        react app
+        </br>
+      </h1>
     </div>
   );
 }
 
 export default App;
+
+
+
